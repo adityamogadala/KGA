8f71e65f9c4c9e2efea9901b94d014f9533c33ba
Aditya Mogadala <>
2018-05-12 02:34:34 +1000

    update embeddings

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
 import numpy as np
 
 from keras.preprocessing import sequence
@@ -11,20 +10,16 @@ max_caption_length = 25
 def textdata(emb,dataset,type_feat,lang,vecdb):
 	s=shelve.open(vecdb)
 	unique=[]
-        with open('../vocabulary.txt') as f:
+        with open('../vocablist/vocabulary.txt') as f:
                 for line in f:
                         unique.append(line.strip('\n'))
         vocab_size = len(unique)
-        #unique = list(set(s.keys()))
-        #vocab_size = len(unique)
-	print(vocab_size)
 	word_index = {}
 	index_word = {}
 	for i,word in enumerate(unique):
 	    word_index[word] = i
 	    index_word[i] = word
 	embedding_matrix = np.zeros((vocab_size,emb))
-	#embedding_matrix = np.random.uniform((vocab_size,emb))
 	count=0
         for word, i in word_index.items():
 		try:
@@ -37,8 +32,7 @@ def textdata(emb,dataset,type_feat,lang,vecdb):
 			print(word)
 			pass
         s.close()
-	print(count)
-	np.save('../seqstoretraindata/'+dataset+'/embedding_matrix.512.'+lang+'.npy',embedding_matrix)
+	np.save('../seqstoretraindata/embedding_matrix.npy',embedding_matrix)
 def main():
 	#emb=256
 	emb=512
@@ -46,8 +40,7 @@ def main():
 	dataset_coco='rmeightmscoco'
 	type_vgg = 'vgg16'
 	type_res = 'resnet50'
-	#vecdb="/data2/u1031879/kga/shelvedbs/rmeightmscoco/256.en.rmeightmscoco.db"
-	vecdb="/data2/u1031879/captiongen/shelvedbs/karpathycoco/glove.512.en.karpathycoco.db"
+	vecdb="glove.512.en.karpathycoco.db"
 	textdata(emb,dataset_coco,type_vgg,lang,vecdb)
 if __name__=='__main__':
 	main()
